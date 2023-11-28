699b437aa472d233e541dfe84bc947c0cd3906cb
Louis-C7 <972339262@qq.com>
2023-11-28 14:57:46 +0800

    openharmony sig format

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { Platform } from "react-native";
-import LinearGradientIos from "./index.ios.js";
-import LinearGradientAndroid from "./index.android.js";
-import LinearGradientWindows from "./index.windows.js";
+import LinearGradientIos from "react-native-linear-gradient/index.ios.js";
+import LinearGradientAndroid from "react-native-linear-gradient/index.android.js";
+import LinearGradientWindows from "react-native-linear-gradient/index.windows.js";
+import LinearGradientHarmony from "./index.harmony.js";
 
 export const LinearGradient = Platform.OS === "ios"
   ? LinearGradientIos : Platform.OS === "android"
   ? LinearGradientAndroid : Platform.OS === "harmony"
-  ? LinearGradientAndroid : LinearGradientWindows;
+  ? LinearGradientHarmony : LinearGradientWindows;
 
 export default LinearGradient;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import LinearGradient from './RNLinearGradientNativeComponent';
+import LinearGradient from 'react-native-linear-gradient/src/RNLinearGradientNativeComponent';
 
 export default LinearGradient;
-export {default as LinearGradient} from './RNLinearGradientNativeComponent';
-export * from './RNLinearGradientNativeComponent';
+export {default as LinearGradient} from 'react-native-linear-gradient/src/RNLinearGradientNativeComponent';
+export * from 'react-native-linear-gradient/src/RNLinearGradientNativeComponent';
